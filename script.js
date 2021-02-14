b0fe7660ac2a82933f93657d3d1e0ff6285460f0
Nikola Mirilo <nikolamirilo@gmail.com>
2021-02-14 15:36:24 +0100

    added darktheme

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const p = document.querySelector('.text');
-function popupf (){
-alert('This alert message is written in JS!')
+function popupf() {
+  console.log('This alert message is written in JS!')
 }
 alertfunction = () => {
-p.addEventListener('click', popupf);
-  }
+  p.addEventListener('click', popupf);
+}
 alertfunction();
 const h = document.querySelector('h1');
-hello = () =>{
-    console.log(h,p);
+hello = () => {
+  console.log(h, p);
 }
